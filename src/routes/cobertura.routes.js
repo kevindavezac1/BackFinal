b66d9ab297031200be9a4e5f14f1b426e4b414ff
Kevin Davezac <kevindavezac@outlook.com>
2024-12-18 00:38:44 -0300

    back actualizado

--- src/routes/cobertura.routes.js
+++ src/routes/cobertura.routes.js
@@ -7,6 +7,7 @@ router.get('/coberturas', coberturaController.getCoberturas);
 router.post('/coberturas', coberturaController.createCobertura);
 router.put('/coberturas/:id', coberturaController.updateCobertura);
 router.delete('/coberturas/:id', coberturaController.deleteCobertura);
-router.get('/coberturas/:id', coberturaController.getCoberturaDelUsuario);
+router.get('/coberturas/usuario/:id', coberturaController.getCoberturaDelUsuario);
+
 
 export default router;
